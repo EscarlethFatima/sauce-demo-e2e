b505436aa32808c161895bbc60f4356ebb596e4f
EscarlethFatima <escarlita.888@gmail.com>
2026-01-29 18:46:38 +0100

    Product Catalog spec

---
+++ pages/inventory.item.page.ts
@@ -0,0 +1,20 @@
+import { Page, Locator } from '@playwright/test';
+
+export class InventoryItemPage {
+  readonly page: Page;
+
+  readonly name: Locator;
+  readonly description: Locator;
+  readonly price: Locator;
+  readonly image: Locator;
+  readonly addToCartButton: Locator;
+  constructor(page: Page) {
+    this.page = page;
+
+    this.name = page.getByTestId('inventory-item-name');
+    this.description = page.getByTestId('inventory-item-desc');
+    this.price = page.getByTestId('inventory-item-price');
+    this.image = page.locator('.inventory_details_img');
+    this.addToCartButton = page.getByTestId('add-to-cart');
+  }
+}
---
+++ pages/product-catalog.page.ts
@@ -0,0 +1,56 @@
+import { Page, Locator, expect } from '@playwright/test';
+
+export class ProductCatalogPage {
+    readonly page: Page;
+
+    readonly inventoryContainer: Locator;
+    readonly inventoryItems: Locator;
+
+    readonly itemName: (item: Locator) => Locator;
+    readonly itemDescription: (item: Locator) => Locator;
+    readonly itemPrice: (item: Locator) => Locator;
+    readonly itemImage: (item: Locator) => Locator;
+    readonly addToCartButton: (item: Locator) => Locator;
+    readonly sortDropdown: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.inventoryContainer = page.getByTestId('inventory-container');
+        this.inventoryItems = page.getByTestId('inventory-item');
+
+        this.itemName = (item: Locator) =>
+            item.getByTestId('inventory-item-name');
+
+        this.itemDescription = (item: Locator) =>
+            item.getByTestId('inventory-item-desc');
+
+        this.itemPrice = (item: Locator) =>
+            item.getByTestId('inventory-item-price');
+
+        this.itemImage = (item: Locator) =>
+            item.locator('img');
+
+        this.addToCartButton = (item: Locator) =>
+            item.locator('[data-test^="add-to-cart"]');
+        this.sortDropdown = page.getByTestId('product-sort-container');
+    }
+    async getAllProductNames(): Promise<string[]> {
+        return this.page
+            .getByTestId('inventory-item-name')
+            .allTextContents();
+    }
+    async getAllProductPrices(): Promise<number[]> {
+        const prices = await this.page
+            .getByTestId('inventory-item-price')
+            .allTextContents();
+
+        return prices.map(price => Number(price.replace('$', '')));
+    }
+    async sortBy(value: 'az' | 'za' | 'lohi' | 'hilo') {
+        await this.sortDropdown.selectOption(value);
+    }
+    async openProductDetails(item: Locator) {
+        await this.itemName(item).click();
+        await expect(this.page).toHaveURL(/inventory-item/);
+      }
+}
